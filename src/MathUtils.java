5682a9b44177df001afe614ab2973c405402fdd6
Tim HOANG <ht.nghiem0306@gmail.com>
2022-11-23 14:40:53 +0100

    update test

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -8,5 +8,22 @@ public class MathUtils {
     public int substract(int a, int b){
         return a-b;
     }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public int divide(int a, int b){
+        return a/b;
+    }
+    public boolean isEven(int number){
+        return number%2==0;
+    }
+
+    public boolean isOdd(int number){
+        return number%2!=0;
+    }
+
+    public  int power(int number,int exp){
+        return number*number;
+    }
 }
 
--- test/MathUtilsTest.java
+++ test/MathUtilsTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -71,13 +74,50 @@ class MathUtilsTest {
     }
 
     @DisplayName("Tester la soustraction de 2 nombres.")
-    @Test // pour considerer le methode en dessous comme un test
-    void testSubstract() {
-        var nb1 = 10;
-        var nb2 = 6;
-        var expectedResult = 4;
-        assertEquals(4, mathUtils.substract(nb1, nb2), String.format("%d - %d doit etre" +
+    @ParameterizedTest //test avec plusieurs parametres externes qui permet d'eviter les
+    //duplications
+    @CsvFileSource(resources = "/soustraction.csv") //utiliser les parametre d'une fichier csv
+        // importer les file csv
+        // creer un package "resources" dans le fichier test
+        // test_unitaire => (click right) open module setting, select resources and select test resource.
+    void testSubstract(int nb1, int nb2, int expectedResult) {
+        assertEquals(expectedResult, mathUtils.substract(nb1, nb2), String.format("%d - %d doit etre" +
                 "egal a %d",nb1,nb2,expectedResult));
-        // le message dans string format n'affiche que dans le case test echoue
+        // le message dans string.format n'affiche que dans la case test echoue
     }
+
+
+    @DisplayName("Tester le produit de 2 valeurs")
+    @ParameterizedTest //test avec plusieurs parametres externes qui permet d'eviter les
+    //duplications
+    @CsvSource({
+            "4,2,8",
+            "50,2,100",
+            "45,2,90",
+            "8,8,64"
+    }) // les parametres de type dans une fichier csv
+    void testMultiply(int nb1,int nb2, int expected) {
+        assertEquals(expected,mathUtils.multiply(nb1,nb2));
+    }
+
+    @DisplayName("Tester le rapport de 2 valeurs")
+    @Test
+    void testdivide() {
+        assertEquals(2,mathUtils.divide(4,2));
+    }
+    @DisplayName("Tester si un nombre est pair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/is_even.csv")
+    void TestIsEven(int number, boolean expectedResult) {
+        assertEquals(expectedResult,mathUtils.isEven(number));
+    }
+
+    @DisplayName("Tester si un nombre est impair")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/is_odd.csv")
+    void TestIsOdd(int number, boolean expectedResult) {
+        assertEquals(expectedResult,mathUtils.isOdd(number));
+    }
+
 }
+
