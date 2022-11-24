16e093c5c9fa2ebfc62704d28d57b0f1e3aef286
Tim HOANG <ht.nghiem0306@gmail.com>
2022-11-24 09:27:45 +0100

    add test StringUtils

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Locale;
 
 public class StringUtils {
@@ -15,4 +16,138 @@ public class StringUtils {
         }
         return strFormatted.equals(reversed);
     }
-}
+
+        static boolean isPrefix(String str1,String str2){
+            String str1Formatted = str1.replaceAll("[^A-Za-z]", "").toLowerCase();
+            String str2Formatted = str1.replaceAll("[^A-Za-z]", "").toLowerCase();
+            // 2. add formatted and replace str1 and str2 by str1Formatted and str2Formatted to solve test 2
+            if(str1Formatted.length()>str2Formatted.length()){
+                return str1Formatted.substring(0,str2Formatted.length()).equals(str2Formatted);
+            }else{
+                return str2Formatted.substring(0,str1Formatted.length()).equals(str1Formatted);
+            }
+        }
+
+
+        public static boolean isAnagram(String str1,String str2) {
+            String[] str1Format = (str1.replaceAll("[^A-Za-z]", "").toLowerCase().split(""));
+            String[] str2Format = (str2.replaceAll("[^A-Za-z]", "").toLowerCase().split(""));
+
+            Arrays.sort(str1Format);
+            Arrays.sort(str2Format);
+
+            if (str1.length() != str2.length()) {
+                return false;
+            }
+
+//        String building
+            StringBuilder  arrStr1=new StringBuilder();
+            for(String letter:str1Format){
+                arrStr1.append(letter);
+            }
+
+            StringBuilder  arrStr2=new StringBuilder();
+            for(String letter:str2Format){
+                arrStr2.append(letter);
+            }
+//            return (!arrStr1.toString().equals(arrStr2.toString()));
+            return (arrStr1.toString().equals(arrStr2.toString()));
+            // test minimal return false => remove "!"
+
+        }
+
+
+        //    Renverser une chaine : papa => apap
+        public static String reverseString(String str){
+            String reversed="";
+            String strFormatted = str.replaceAll("[^A-Za-z]","").toLowerCase();
+            //add formatted to solve test 2 and test 3
+            for(String letter:strFormatted.split("")){
+                reversed = letter+reversed;
+            }
+            return reversed;
+        }
+
+
+        //    Test si une chaine passé en paramètre est un palindrome
+//        public static boolean isPalindrom(String str) {
+//            String formattedStr=str.replaceAll("[^A-Za-z]","");
+//            String reversedStr=reverseString(formattedStr);
+//
+//            return  formattedStr.equals(reversedStr);
+//        }
+
+        //    Renverser un entier :  10 => 1 , -89 => -98
+        public static int reverseInt(int number){
+            String numberToStr=Integer.toString(number).replaceAll("[^0-9]","");//retirer le "-"
+
+            String reversedStr="";
+            for(String letter:numberToStr.split("")){
+                reversedStr=letter+reversedStr;
+            }
+
+            int reversedToInt=(int) (Integer.parseInt(reversedStr)*Math.signum(number));
+            return reversedToInt;
+        }
+
+
+        //    Mettre toutes les premieres lettre des mots en majuscules
+//    je suis dans la joie => Je Suis Dans La Joie
+        public static String capitalze(String str) {
+            String capitalized=String.valueOf(str.charAt(0)).toUpperCase();
+            String[] strToArr=str.split("");
+            System.out.println(Arrays.toString(strToArr));
+            for(int i=1;i<strToArr.length;i++){
+                if(strToArr[i-1].equals(" ")){
+                    capitalized +=strToArr[i].toUpperCase();
+                }else{
+                    capitalized +=strToArr[i];
+                }
+            }
+            return capitalized;
+        }
+
+        //   [a, b, c, d, e] => [a, B, c, D, e]
+        static char[] alternate(char[] chars){
+            char[] alternateChar=new char[chars.length];
+            alternateChar[0]=chars[0];
+            for (int i = 1; i < chars.length; i ++) {
+                if(chars[i]%2==0){
+                    alternateChar[i]=String.valueOf(chars[i]).toUpperCase().charAt(0);
+                }else{
+                    alternateChar[i]=chars[i];
+                }
+            }
+            return alternateChar;
+        }
+
+
+        // On comment au point [x=0,y=0]
+        // haut : y++
+        // bas : y--
+        // gauche : x--
+        // droite : x++
+        // Exemple : {"haut", "haut", "bas", "gauche", "gauche", "droite", "haut"} => [-1,2]
+        static int[] direction(String[] directionStr){
+            int[] result={0,0};
+            for(int i=0;i<directionStr.length;i++){
+                switch (directionStr[i]){
+                    case "haut":
+                        result[1] +=1;
+                        break;
+                    case "bas":
+                        result[1] -=1;
+                        break;
+                    case "gauche":
+                        result[0] -=1;
+                        break;
+                    case "droite":
+                        result[0] +=1;
+                        break;
+                }
+            }
+            return result;
+        }
+
+    }
+
--- test/StringUtilsTest.java
+++ test/StringUtilsTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringUtilsTest {
@@ -8,12 +11,77 @@ public class StringUtilsTest {
     @DisplayName("Tester si une chaine est un palindrome")
     @ParameterizedTest
     @CsvFileSource(resources = "/palindrome.csv")
-    void testIfStringIsPalindrome(String value, boolean expectedResult){
-        assertEquals(expectedResult,StringUtils.isPalindrome(value));
+    void testIfStringIsPalindrome(String value, boolean expectedResult) {
+        assertEquals(expectedResult, StringUtils.isPalindrome(value));
         // 1. Tester une chaine simple (ana,true)
         // 2. Tester une chaine avec les lettres en majuscules (add Ana,True to the palindrome.csv)
         // 3. Tester une chaine avec l'espace (add an "a,True" and "a n a,true"to palindrom.csv)
         // 4. Tester une chaine avec des charateres speciaux (add " a-n?a,true)
         // Test driven development , tous les tests ont une modification dans le fichier StringUtils
     }
+
+    @DisplayName("Tester si un mot est prefix d'un autre mot")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/is_prefix.csv")
+    void testIfStringIsPrefix(String value1, String value2, boolean expectedResult) {
+        assertEquals(expectedResult, StringUtils.isPrefix(value1, value2));
+
+        //1. test minimal - add abccbc,abc,true to is_prefix.csv
+        //2. test with space, special characters and majuscule - add (ABccbc,abc,true) (ABc cbc,a bc,True)
+        //  ( Abc@ cbc,a!bc,true) to is_prefix.csv
+        //
+    }
+
+    @DisplayName("Tester si 2 mots sont anagram")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/is_anagram.csv")
+    void testAnagram(String value1, String value2, boolean expectedResult) {
+        assertEquals(expectedResult, StringUtils.isAnagram(value1, value2));
+        //1. test minimal - add rage,gare,true to is_anagram.csv
+        //2. test avec espace, special characters and majuscule - add (rag e, ga re,true) add (Rage,gare,true)
+        // and(ra#ge,gare!,true)
+    }
+
+    @DisplayName("Tester reverse un string")
+    @Test
+    void testReverse() {
+        assertEquals("fedcba", StringUtils.reverseString("abcdef"));
+        //1.test minimal with abcdef,fedcba
+        assertEquals("fedcba", StringUtils.reverseString("ABcdef"));
+        //2. test with majuscule
+        assertEquals("fedcba", StringUtils.reverseString("A!B-c def"));
+        //3. test with space and special characters
+    }
+
+    @DisplayName("Tester reverse un entier")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/reverse_int.csv")
+    void testReverseInt(int value, int expectedResult) {
+        assertEquals(expectedResult, StringUtils.reverseInt(value));
+        //1.test minimal add 123,321 and -123,-321 to reverse_int.csv
+        //2. test avec 20,2 -30,3
+
+    }
+
+    @DisplayName("Tester mettre toutes les premieres lettre des mots en majuscules")
+    @ParameterizedTest
+    @CsvSource({
+            "je suis dans la joie,Je Suis Dans La Joie"
+    })
+    void testCapitalize(String value, String expectedResult) {
+        assertEquals(expectedResult, StringUtils.capitalze(value));
+        //1. test minimal, add je suis dans la joie, Je Suis Dans La Joie to capitalize.csv
+    }
+
+    @DisplayName("Tester capitalizer les lettrer Alternativement")
+    @Test
+    void testAlternate() {
+        assertArrayEquals(new char[]{'a', 'B', 'c', 'D', 'e'},StringUtils.alternate(new char[]{'a', 'b', 'c', 'd', 'e'}));
+    }
+    @DisplayName("Tester la direction")
+    @Test
+    void testDirection() {
+        assertArrayEquals(new int[]{-1,2},StringUtils.direction(new String[]{"haut", "haut", "bas", "gauche", "gauche", "droite", "haut"}));
+    }
+
 }
